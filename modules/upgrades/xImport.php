5d951fff96988e9029ff35c867df96d0e77f3352
cheelam <cheelam.thenux@gmail.com>
2023-10-13 20:58:52 +0800

    upload

---
+++ modules/upgrades/xImport.php
@@ -0,0 +1,330 @@
+<?php
+
+$repository = array(
+    //"timestamp" => "Descriptions.",
+    
+    /* -- by Chin Teck -- */
+
+    /* -- by Zhi Yan -- */
+
+    /* -- by Jonsan -- */
+    "1643006581" => "Add Pickup Origin Settings and add tariff code column in county table",
+    "1645005440" => "Add acc_closing batch table.",
+    "1646278458" => "Add nice payment payment gateway and settings.",
+   
+    /* -- by Khoon Yew -- */
+    "1643091381" => "Add Special Rank to Receive Special Percentage.",
+    "1643166760" => "Tune client_rank_monthly table index.",
+    "1643352720" => "Add Started Kit checking for Entreprenuer Rank",
+    "1644288431" => "Add Special Calculate Sales Setting.",
+    "1644462730" => "Add minActiveLeg requirement.",
+    "1644837640" => "Add Team Bonus Setting.",
+    "1644806360" => "Add Leadership bonus tuning.",
+    "1647420352" => "Add extend Option duration setting.",
+    "1647837603" => "Add new column to store tax Percentage.",
+    "1648032904" => "Add new Table and new column for Month Performance Report.",
+    "1648098856" => "End Recruit Promo On end of March.",
+    "1649760950" => "Update Bonus Tax Percentage Setting.",
+    "1649901987" => "Add permission for Bonus Tax Setting.",
+
+    /* -- by Roc -- */
+    "1650424590" => "Tune api param not compulsory for add ticket",
+
+    /* -- by Chris Ting -- */
+    "1643164390" => "Update company contact.",
+    "1644992530" => "Add weight column in mlm_product",
+    "1645502702" => "Add inv_voucher tables",
+
+    /* -- by KaiXiang -- */
+    "1643016114" => "Closed Delivery Charges Setting and Listing",
+    "1643353749" => "Rearrange Admin Side Bar",
+    "1643353832" => "Add Session Setting and Keep Login Module Setting",
+    "1643353856" => "Add session_data table for handle temporary data",
+    "1644200067" => "Add inventory editable admin roles flag in system settings",
+    "1644462120" => "Add index for inventory module table",
+    "1644482203" => "Add courrier cloumn in inv_order",
+    "1647244685" => "Add back batch_id column in inv_delivery_order",
+    "1648539516" => "Add state, sub_county and zip code table index and column",
+    "1648697861" => "Add courrier company in inv order table and destination id in zip code table",
+
+    /* -- by Yi Xuan -- */
+    "1645413571" => "Add type column for memo",
+    "1646198889" => "Add in side bar for Bonus Payout Summary (Monetary)",
+
+    /* -- by Wen Le -- */
+    "1643246106" => "remove the Sales Placement Report in side bar",
+    "1645516109" => "add menu for discount voucher setting",
+    "1646038634" => "add menu for discount voucher redemption setting",
+    "1646199010" => "rename all Goldmine Bonus to Personal Group Bonus",
+    "1647479912" => "rearrangement for admin menu side bar",
+
+    /*by Sanuel*/
+    "1645154662" => "remove Placement Tree page",
+    "1654661964" => "Disable Chinese Simplified from Language",
+    "1661410378" => "Upgrade system rank for Helvianri",
+    "1660558419" => "add metafizBonusWallet for Bonuses",
+    "1661490161" => "set current rank and client rank table to old and add new",
+    "1661960233" => "reopen Placement Tree permission",
+    "1662018298" => "Disable all old bonus in mlm_bonus",
+    "1662456020" => "Remove mlm_bonus as table reference for all old bonus side bar permissions",
+    "1663064618" => "Open Admin site Adjustment In/Out permission for bonus wallet",
+    "1663647665" => "Add isPurchaseCredit to Metafiz Wallet",
+    "1663142784" => "Unhide Admin site Withdrawal Page",
+    "1663231763" => "Set Bonus Wallet withdrawal admin charge Percentage to 0",
+    "1663647944" => "Update Bonus Wallet withdrawal accept type to bank",
+    "1663916891" => "Add bank_city column to mlm_withdrawal",
+    "1663924861" => "Add permission for Business Basic Module",
+
+    //sinsin
+    "1656404553" => "add Permission for new active recruit",
+    "1656560205" => "add product setting for cashback",
+    /* Ong */
+    "1654069896" => "add column discount amount to table mlm_pending_payment",
+
+    /*by Joe*/
+    "1663150871" => "Update system language from for Leadership Award Bonus to Leadership Cash Reward",
+
+    /* wenbin */
+    "1654677540" => "add email attachment  table",
+
+    /* Bruce */
+    "1667453297" => "Metafiz - Insurance Charges: add column insurance_tax in inv_order",
+
+    /* Zhen Hao */
+    "1678845072" => "Create Vendor and Vendor Address",
+    "1679030254" => "Create Product",
+    "1679302079" => "Create Product Category",
+    "1679471701" => "Create Attribute and Attribute Value",
+    "1679539703" => "Alter product table change expired date to expired day, sale price to margin percentage",
+    "1679647958" => "Create product template and product media, alter product",
+    "1680174582" => "drop quantity column in product",
+    "1680969438" => "Create product item and change product margin percen to sale price",
+
+    /* Bq Chiew */
+    "1679048563" => "Create Shop Table",
+    "1679477405" => "Create Assign_Shop Table",
+    "1679979965" => "Create Device Table",
+
+    /* Lee Yang */
+    "1679364206" => "Create Purchase Request and Purchase Order",
+    "1680680151" => "Modify and add column for purchase_request table and purchase_order table",
+    "1680692054" => "Create Warehouse and Stock",
+
+    /*-- by TeckLoong --*/
+    "1679363597" => "alter table shopping_cart, rename mlm_product_id to product_id",
+    "1679469848" => "insert FPX data into table provider and provider_setting",
+    "1679477250" => "insert FPX data into table provider and provider_setting",
+    "1679997488" => "insert data into table provider_setting and alter column table uploads",
+    "1680000700" => "alter column table uploads",
+    "1680063339" => "add table saleDetail, sale, payment_gateway_details",
+    "1680168673" => "rename table sale to sale_order, saleDetail to sale_order_detail",
+    "1680225443" => "add column product_template_id, product_id in table sale_order, shopping_cart",
+    "1680230726" => "add column shipping_address, billing_address in table sale_order",
+    "1680232179" => "add record for deliveryFee in table system_settings",
+    "1680339721" => "add record for pamentMethod and deliveryMethod in table system_settings",
+    "1680683486" => "rename column payment_fee to shipping_fee in table system_settings",
+    "1680951524" => "add column deleted in table sale_order_detail",
+    "1680963658" => "add record for companyAddressActual in table system_settings",
+    "1681092317" => "add column sale_id in table shopping_cart",
+    "1681353301" => "add column Created_by in table purchase_request",
+
+    /* herman */
+    "1686136896" => "create table name product_wishlist",
+    "1686552040" => "insert data to table name permission",
+    "1686555669" => "insert subData to table name permission",
+    "1686555616" => "insert data to table name role permission",
+    "1686632666" => "create table name product_favorite",
+    "1686704646" => "create table name product_review",
+    "1686881189" => "Modify and add column for product_review",
+    "1687164823" => "insert data to table name permission",
+    "1687165709" => "insert data to table name role_permission",
+    "1687166214" => "insert data to table name permission for customerDetail",
+    "1687166309" => "insert data to table name role_permission for customerDetail",
+    "1687510877" => "insert data to tables name provider and message_code",
+    "1687510879" => "query correction for insert data to tables name provider and message_code",
+    "1687838875" => "insert data to table name process",
+    "1688349213" => "insert default quantity for low Stock Notification",
+    "1688353128" => "edit column name to insert data to system_setting",
+    "1688362327" => "insert data to provider,provider_setting and message_code",
+
+    /* Chee Lam */
+    "1679370085" => "Modify Address table city_id column able to accept string",
+    "1679377353" => "Import Malaysia State into state table",
+    "1680489642" => "Tune address table - rename city_id and post_code_id",
+    "1680926634" => "Add new column 'buying date' in purchase_request table",
+    "1680936448" => "Create new table 'purchase_product'",
+    "1680941116" => "Add new column 'product_name' for purchase_product table",
+    "1680963909" => "Change Purchase Request table column - product id",
+    "1680964853" => "Add new column 'purchase_order' in purchase_product table",
+    "1680966691" => "Add new column 'remarks' in purchase_product table",
+    "1681052264" => "Update System_Settings minPasswordLength from 6 to 8",
+    "1681092614" => "Create Delivery Method table",
+    "1681184568" => "Add new page 'edit Sale Order'",
+    "1681202998" => "Create new table 'serial_number'",
+    "1681282538" => "Add new column 'remarks' for purchase_order table",
+    "1681286166" => "Add new column 'type' for purchase_product table",
+    // "1681358187" => "Tune purchase request table missing column",
+    "1681700877" => "Add new column 'warehouse_id' in purchase_request table",
+    // "1681703636" => "Create new table - admin_setting",
+    "1681797944" => "Add Created_by column in purchase_order table",
+    "1681873479" => "Change stock table expiration_date column to date",
+    "1681897501" => "Insert message code in message_code table",
+    "1681955072" => "Change provider_setting table, add auto_increment on id",
+    "1682494294" => "Add new column tracking_no for sale_order table",
+    "1682652698" => "Change sale_order table status from small capital to capital letter",
+    // "1682677431" => "Crete new table 'client_sale_order'",
+    "1682686729" => "Create new table 'so_tracking'",
+    "1682686952" => "Change tracking_no to tracking_id for sale_order table",
+    "1683023340" => "Create new table 'guest_token'",
+    "1683024305" => "Add new column 'token' for shopping_cart table",
+    // "1683089993" => "Add new column 'token' for sale_order_detail table",
+    // "1683091960" => "Add new column 'token' for sale_order table",
+    "1683097223" => "Add new column 'sale_id' for guest_token table",
+    "1683522916" => "Fixed bonus wallet menu",
+    "1683859716" => "Enable back package page",
+    "1684141217" => "add new column for package_item table",
+    "1684906209" => "create new table 'favourite_list'",
+    "1684912425" => "add new column 'sales_count' in product table",
+    "1685602938" => "add new column 'vendor_address' in purchase_request table",
+    "1685608053" => "add new column 'vendor_address' in purchase_order table",
+    "1685701347" => "Add new table 'purchase_media'",
+    "1686188783" => "Modify promo code system_settings, add coming soon in value",
+    "1686207508" => "Add column in mlm_promo_code table",
+    "1686212778" => "Create new table 'promo_code_detail' table",
+    "1686277991" => "Modify mlm_promo_code table and drop column from promo_code_detail table",
+    "1686541013" => "modify column for mlm_promo_code and drop column from promo_code_detail table",
+    "1686901745" => "Move discount percentage from promo detail table to main table",
+    "1687323133" => "modify so_service table column from int become decimal",
+    "1687426833" => "add new table 'promo_code_product'",
+    "1687429095" => "add new column 'sale_price' in promo_code_product table",
+    "1688045484" => "add new column in stock_move_line table",
+    "1688362424" => "add new table 'goTasty_enquiry'",
+    "1688363445" => "insert message code",
+    "1688457679" => "change gotasty warehouse address",
+    "1688460019" => "Create new table 'goTasty_utm_history'",
+    "1688461785" => "Add new column 'view_count' in product table",
+    "1688610188" => "add new column 'phone_number' in goTasty_enquiry table",
+    "1688610872" => "add new table career_media",
+    "1688612490" => "insert new message code",
+    "1689051413" => "create new table outgoing_webservices",
+    "1689069677" => "insert into provider table",
+    "1689132773" => "add new column 'ignore stock count'",
+    "1689157377" => "insert new page, review listing review detail",
+    "1689216754" => "create new table 'pricelist' and 'pricelist_detail'",
+    "1689243551" => "Sync permission and role permission",
+    "1689243953" => "Fixed PR page title",
+    // "1689244789" => "Fixed edit PR Page title",
+    "1690169367" => "add new column in mlm_promo code table, and drop column from promo_code_detail",
+    "1690173913" => "add new column promo code name in mlm_promo_code",
+    "1690190254" => "add new column in mlm_promo code table and drop column from promo_code_detail 2",
+    "1690253696" => "Add new column in mlm_promo code, max_quantity",
+    "1690345895" => "Add new column in mlm_promo_code, is_first_time_purchase",
+    "1690432306" => "Create new table 'sale_order_item'",
+    "1690433644" => "Create new page 'Credit refund'",
+    "1690434731" => "Change sale_order_item column name",
+    "1690451090" => "insert FPX mepsfpx link into system_setting",
+    "1690538910" => "Create new page for print serial",
+    "1688465220" => "add new column in utm record table",
+    "1690777001" => "add quantity column in sale_order_item table",
+    "1690806410" => "add new column in sale_order_detail",
+    "1690868601" => "modify inv_delivery_order and inv_delivery_order_detail table",
+    "1690943137" => "modify sale_order_item column",
+    "1690964989" => "add new column in inv_delivery_order table",
+    "1691384460" => "add new column in delivery_method_detail",
+    "1691411840" => "add new column serial_number,do_no, in sale_order_item",
+    "1691467358" => "add new column apply_condition in delivery_method_detail table",
+    "1691492054" => "add new provider setting for whallo and parcelhub",
+    "1691548992" => "Insert new message code for write to us and career application",
+    "1691558035" => "add new message code and new message assigned",
+    "1691641891" => "add new table delivery_method_postcode",
+    "1691740384" => "disable Package Delivery Charges option from gotasty_delivery_method",
+    "1692160986" => "create change_item_request table, add new column for sale_order_item table",
+    "1692667505" => "Create new table gotasty_payment_method",
+    "1692765698" => "insert provider and provier_setting for parcelhub cancel",
+    "1692932940" => "Change from range and to range column in bigint",
+    "1693209099" => "add new page for PR PO combine",
+    "1693216610" => "new purchase order table",
+    "1693379929" => "add new column for purchase order 'assign_to'",
+    "1693899731" => "change purchase order table created by and approved by column to varchar",
+    "1693985383" => "create new table po_assign table",
+    "1693992297" => "drop assign_to column from purchase_order",
+    "1694053895" => "add index for po_assign table",
+    "1694142291" => "add 'rack_no' in stock table",
+    "1694491352" => "add column for vendor_address table",
+    "1695034303" => "add column for purchase_order table",
+    "1695357137" => "add column for product and stock table",
+    "1696326140" => "change purchase_order purchase_date from datetime to date",
+    "1697020988" => "add uploads table journal",
+    "1697021457" => "add so column name into enumerators table",
+    "1697091152" => "add language code into enumerators table",
+    "1697095214" => "insert sale_order_item table for audit",
+    "1697160894" => "add product and product_media into journal table",
+    "1697190226" => "change Uploads Table into DO",
+
+    /* Sam */
+    "1680156089" => "client table add new column to define encryption method",
+    "1680772047" => "alter po_request and pr_request and add pr_order_line table",
+    
+    /* Marcus */
+    "1682387137" => "Add new table cooking_suggestion and table cooking_suggestion_details. Add records in roles_permission and permission table",
+    "1684488490" => "Add new column branch_name in table vendor_address",
+    "1684814881" => "Add new column description and parent_id in table product_category",
+
+    /* Chong Wei */
+    "1683265215" => "Add Remark behind url column at cooking_suggestion_details",
+    "1681980850" => "Add new records in credit and credit settings",
+    "1683284043" => "Add URL at cooking templates table",
+    "1684488556" => "Add shipping and billing email at Sale Order Table",
+    "1684815884" => "Add cooking time at Cooking Suggest Table",
+    "1684815885" => "Change decsription text character",
+    "1684815886" => "Change cooking_suggestion text character",
+    "1685006754" => "Create vendor media table",
+    "1686021169" => "Alter product description to text",
+    "1686220055" => "Alter sale order table delete column",
+    "1688542339" => "Add telegram to system settings",
+    "1688630374" => "Add OTP count requests to system settings",
+    "1689066822" => "Add Order Number Column for Purchase Request Table",
+    "1689321712" => "Add dry deliver charges to gotasty_delivery_method table and system setting table",
+    "1689324205" => "Add delivery method column to product table and insert 2 into each record",
+    "1690345982" => "Add Redeem Point Rate to system settings",
+    "1691135464" => "Add Bearer into provider setting",
+    "1691145160" => "Add Delivery Order into permission table",
+    "1691207784" => "Add delivery order details into permission table",
+
+
+
+    /* Li Heng */
+    "1689238675" => "Change email provider and add Order Status URL in the System_setting table",
+    "1689306674" => "Add new Attribute for sale_order(is_gift) and address(address_2)",
+    "1689323090" => "Add address details column in database for sale_order",
+    "1690172896" => "Add cost into package_item table",
+    "1690429959" => "Drop original gotasty_delivery_method and create new gotasty_delivery_method and delivery_method_detail table and insert data",
+    "1690522714" => "Add new free delivery method for package",
+    "1690880993" => "Add new anonumous column for product review table",
+    "1691632651" => "update permission table for pricelist listing breadcrumb",
+    "1691762388" => "add new column in sale_order rewarded point",
+
+    // jimmy
+    "1683531470" => "create mlm_promo_code",
+    "1683616573" => "create mlm_bonus_direct_sponsor",
+
+    "1693403410" => "create action_permission",
+
+    //winson
+    "1693450869" => "Add audit id column to _audti table",
+
+    //soon
+    "1693895734" => "Create provider_id column in message_assigned",
+
+    //ZiJac
+    "1693890937" => "Add postcode page and edit postcode page",
+    //darren
+    "1694183810" =>"Create gt_banner table for menu banner",
+    "1694183990" =>"Create gt_banner_image table for menu banner",
+    "1694184120" =>"Add new menu in permissions and roles_permission for gotasty menu banner into sidebar",
+    "1694362090" =>"Add page for menu banner in permissions and roles_permission"
+);
+
+ksort($repository);
+    
+?>
